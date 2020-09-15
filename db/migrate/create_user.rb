602d441e24445f65efc8290d3f937e73fd841f31
Celina Jackson <celinanjackson@gmail.com>
2020-09-15 19:29:19 +0000

    Automatically backed up by Learn

--- db/migrate/create_user.rb
+++ db/migrate/create_user.rb
@@ -1,7 +1,9 @@
 class CreateUser < ActiveRecord::Migration[5.2]
-  create_table :Users do |t|
-    t.string :name 
-    t.string :email 
-    t.string :password
+  def change 
+    create_table :users do |t|
+     t.string :name 
+     t.string :email 
+     t.string :password
+    end 
   end 
 end
